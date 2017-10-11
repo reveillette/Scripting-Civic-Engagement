11a4895dd1063a9faefbff6564c580690b1c1700
reveillette <reveillette@gmail.com>
2017-10-11 18:27:00 -0400

    Removed SCE examples

--- Sample Files/170907/leaflet-map/map.js
+++
@@ -1,6 +0,0 @@
-var map = L.map('map').setView([38.051,-78.474], 13);
-
-L.tileLayer('https://api.mapbox.com/styles/v1/reveillette/cj70uedyj042q2sp4ty33c11p/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1IjoicmV2ZWlsbGV0dGUiLCJhIjoidmZvYW11SSJ9.2WIhYoRgi7LZF1zOS2xUoA', {
-    attribution: 'Map data: Mapbox Streets v7. Transit data: City of Charlottesville.',
-    maxZoom: 22,
-}).addTo(map);
--- Sample Files/170914/mapbox-gl-map/map.js
+++
@@ -1,9 +0,0 @@
-mapboxgl.accessToken = 'pk.eyJ1IjoicmV2ZWlsbGV0dGUiLCJhIjoidmZvYW11SSJ9.2WIhYoRgi7LZF1zOS2xUoA';
-
-var map = new mapboxgl.Map({
-	container: 'mapboxmap',
-	minZoom: 10,
-	maxZoom: 17,
-	pitchWithRotate: false,
-	style: 'mapbox://styles/reveillette/cj70uedyj042q2sp4ty33c11p' 
-});
